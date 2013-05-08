dfecf0bc2ac12afd4096c6f5e1ebfc0d5ae18bb3
Mitchell Stewart <personal.mitchellstewart@gmail.com>
2013-05-08 12:19:28 +1000

    Added Project
    Added the project to the repository

---
+++ strapdash/strapdash/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace bootstapDash
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-1.9.1.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/knockout-2.2.1.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/bootstrap-responsive.min.css"));
+
+            // The default behaviour is for the IgnoreList to ignore any minified scripts.
+            // Clearing the IgnoreList will include our .min files.
+            bundles.IgnoreList.Clear();
+        }
+    }
+}
---
+++ strapdash/strapdash/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bootstapDash.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET : /
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+
+        //
+        // GET: /Search?query=
+        public string Search(string query)
+        {
+            return "[\"#30 - Stephen Curry\", \"#5 - Kevin Garnett\", \"#24 - Kobe Bryant\", \"#21 - Tim Duncan\", \"#9 - Rajon Rondo\"]";
+        }
+
+    }
+}
